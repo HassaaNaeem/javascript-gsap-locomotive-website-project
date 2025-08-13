f7ace860031df149be6b51a7985cb70f1b46c64f
HassaaNaeem <hassaan282006@gmail.com>
2025-08-14 01:25:19 +0500

    refactored

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
-let tl = gsap.timeline();
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.4,
-});
-tl.from("#line1-part1", {
-  opacity: 0,
-  onStart: function () {
-    const timer = document.querySelector("#line1-part1 h5");
+function loadingAnimation() {
+  let tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.4,
+  });
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      const timer = document.querySelector("#line1-part1 h5");
 
-    let count = 10; // we dont want to show user 0 - 9
-    console.log(timer);
+      let count = 10; // we dont want to show user 0 - 9
+      console.log(timer);
 
-    let interval = setInterval(function () {
-      count++;
-      timer.textContent = count;
-      if (count == 100) {
-        clearInterval(interval);
-      }
-    }, 34);
-  },
-});
-tl.to(".line h2", {
-  animationName: "animate",
-  opacity: 1,
-});
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 2.8,
-});
-tl.from("#page1", {
-  y: 1600,
-  opacity: 0,
-  delay: 0.2,
-  duration: 0.5,
-  ease: Power4,
-});
-tl.to("#loader", {
-  display: "none",
-});
+      let interval = setInterval(function () {
+        count++;
+        timer.textContent = count;
+        if (count == 100) {
+          clearInterval(interval);
+        }
+      }, 34);
+    },
+  });
+  tl.to(".line h2", {
+    animationName: "animate",
+    opacity: 1,
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 2.8,
+  });
+  tl.from("#page1", {
+    y: 1600,
+    opacity: 0,
+    delay: 0.2,
+    duration: 0.5,
+    ease: Power4,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+loadingAnimation();
