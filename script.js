2adb44434df111edd4135c494c807110b2340bdb
HassaaNaeem <hassaan282006@gmail.com>
2025-08-18 22:57:23 +0500

    updates

--- script.js
+++ script.js
@@ -98,7 +98,6 @@ function loadingAnimation() {
     "-=1"
   );
 }
-loadingAnimation();
 
 function cursorAnimation() {
   document.addEventListener("mousemove", function (e) {
@@ -110,5 +109,46 @@ function cursorAnimation() {
 
   Shery.makeMagnet("#menu h3");
 }
+loadingAnimation();
 cursorAnimation();
 locomotiveAnimation();
+
+function sheryAnimation() {
+  Shery.imageEffect(".image-div", {
+    style: 5,
+    config: {
+      a: { value: 0, range: [0, 30] },
+      b: { value: -0.77, range: [-1, 1] },
+      zindex: { value: -9996999, range: [-9999999, 9999999] },
+      aspect: { value: 0.6312707097171919 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: false },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: false },
+      maskVal: { value: 1, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 1 },
+      noise_speed: { value: 0.15, range: [0, 10] },
+      metaball: { value: 0.38, range: [0, 2] },
+      discard_threshold: { value: 0.5, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.51, range: [0, 2] },
+      noise_scale: { value: 10, range: [0, 100] },
+    },
+    debug: true,
+    gooey: true,
+  });
+}
+sheryAnimation();
