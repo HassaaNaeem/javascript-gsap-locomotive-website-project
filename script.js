507b26da01409323de1ef2c5fee2656efdf3a5ce
HassaaNaeem <hassaan282006@gmail.com>
2025-08-25 13:31:21 +0500

    update

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ function locomotiveAnimation() {
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
+    multiplier: 1.5, // makes each wheel delta stronger
+    inertia: 0.9, // smoother but more responsive
   });
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
 
-  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  // tell ScrollTrigger to use these proxy methods for the "#main"  element since Locomotive Scroll is hijacking things
   ScrollTrigger.scrollerProxy("#main", {
     scrollTop(value) {
       return arguments.length
@@ -117,10 +119,10 @@ function sheryAnimation() {
   Shery.imageEffect(".image-div", {
     style: 5,
     config: {
-      a: { value: 0, range: [0, 30] },
-      b: { value: -0.77, range: [-1, 1] },
+      a: { value: 1.83, range: [0, 30] },
+      b: { value: -0.95, range: [-1, 1] },
       zindex: { value: -9996999, range: [-9999999, 9999999] },
-      aspect: { value: 0.6312707097171919 },
+      aspect: { value: 0.7817980342769784 },
       ignoreShapeAspect: { value: true },
       shapePosition: { value: { x: 0, y: 0 } },
       shapeScale: { value: { x: 0.5, y: 0.5 } },
@@ -134,20 +136,19 @@ function sheryAnimation() {
       durationOut: { value: 1, range: [0.1, 5] },
       durationIn: { value: 1.5, range: [0.1, 5] },
       displaceAmount: { value: 0.5 },
-      masker: { value: false },
-      maskVal: { value: 1, range: [1, 5] },
+      masker: { value: true },
+      maskVal: { value: 1.15, range: [1, 5] },
       scrollType: { value: 0 },
       geoVertex: { range: [1, 64], value: 1 },
       noEffectGooey: { value: true },
       onMouse: { value: 1 },
       noise_speed: { value: 0.15, range: [0, 10] },
-      metaball: { value: 0.38, range: [0, 2] },
+      metaball: { value: 0.46, range: [0, 2] },
       discard_threshold: { value: 0.5, range: [0, 1] },
       antialias_threshold: { value: 0, range: [0, 0.1] },
       noise_height: { value: 0.51, range: [0, 2] },
       noise_scale: { value: 10, range: [0, 100] },
     },
-    debug: true,
     gooey: true,
   });
 }
